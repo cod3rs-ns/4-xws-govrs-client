e2727088eddfbf3917700e5612bdd213fee9285b
Stefan Ristanović <st.keky@gmail.com>
2017-01-13 03:45:19 +0100

    PropertySheet fixes

---
+++ src/main/java/rs/acs/uns/sw/govrs/client/fx/editor/property_sheet/PartEnumPropertyItem.java
@@ -0,0 +1,64 @@
+package rs.acs.uns.sw.govrs.client.fx.editor.property_sheet;
+
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.value.ObservableValue;
+import org.controlsfx.control.PropertySheet;
+import rs.acs.uns.sw.govrs.client.fx.serverdomain.enums.PartRoles;
+
+import java.util.Optional;
+
+public class PartEnumPropertyItem implements PropertySheet.Item {
+    private String category;
+    private String name;
+    private String description;
+    private boolean editable;
+    private ObjectProperty<PartRoles> property;
+
+    public PartEnumPropertyItem(ObjectProperty property, String category, String name, String description, boolean editable) {
+        this.property = property;
+        this.category = category;
+        this.description = description;
+        this.name = name;
+        this.editable = editable;
+    }
+
+    @Override
+    public Class<?> getType() {
+        return PartRoles.class;
+    }
+
+    @Override
+    public String getCategory() {
+        return category;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public Object getValue() {
+        return property.get();
+    }
+
+    @Override
+    public void setValue(Object value) {
+        property.set((PartRoles) value);
+    }
+
+    @Override
+    public Optional<ObservableValue<? extends Object>> getObservableValue() {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean isEditable() {
+        return editable;
+    }
+}
